1691f7926f0ba9dd43fbf74787290f0f7d9503ea
ganesh-creator9460 <144310015+ganesh-creator9460@users.noreply.github.com>
2025-09-17 15:15:17 +0530

    Import Scanner class to ger input from user

--- DemoRepoFive/src/com/app/Odd.java
+++ DemoRepoFive/src/com/app/Odd.java
@@ -1,10 +1,13 @@
 package com.app;
-
+import java.util.Scanner;
 public class Odd {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Num : ");
+		int  num = sc.nextInt();
 		
-		for(int i=1;i<=10;i++)
+		for(int i=1;i<=num;i++)
 		{
 			if(i%2!=0)
 			{
